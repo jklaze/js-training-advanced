de608e111f472dab757246e4070d6b1dd85274b3
SWasn003 <ahsan19saeed@gmail.com>
2024-04-18 23:35:27 +0200

    improved design and added enemy movement

--- script.js
+++ script.js
@@ -1,33 +1,67 @@
-let Matrix = 
-[
-    [1,1,1,1,1,1,1],
-    [1,1,1,1,1,1,1],
-    [2,2,2,2,2,2,2],
-    [3,4,3,3,5,3,3],
-];
-//
 let container = document.querySelector(".container");
-console.log(container);
+var enemyPosition = 6;
+var prewEnemyPosition
+
+let Matrix = [
+    [1,1,1,1,1,1,1],
+    [6,6,6,6,6,6,6],
+    [2,2,2,2,2,2,2],
+    [3,4,3,3,3,3,3],
+];
+
+function game(){
+
+    let lastRow = Matrix[Matrix.length-1]
+    
+    enemyPosition >= 6 ? prewEnemyPosition = null : prewEnemyPosition = enemyPosition + 1;
+    if(enemyPosition < 0) enemyPosition = 6
+
+    lastRow.splice(prewEnemyPosition,1,3)
+    lastRow.splice(enemyPosition,1,5)
+    console.log(lastRow);
+
+    enemyPosition = enemyPosition - 1
 
 
-Matrix.forEach((row, rowIndex) => {
-    row.forEach((column, columnIndex) => {
-        let element = document.createElement("div");
-       
-        container.appendChild(element); 
-        element.classList.add("element");
-        if(column === 4 ) Pikachu(element)
-        if(column === 5 ) Hitch(element)
+    BoardGame()
+}
 
-        ambience(column, element)
+function BoardGame(){
+    container.innerHTML = ""
 
+    Matrix.forEach((row, rowIndex) => {
+        row.forEach((column, columnIndex) => {
+            let element = document.createElement("div");
+            element.classList.add("element");
+
+            characters(column,element)
+            ambience(column, element)
+
+            container.appendChild(element); 
+        });
     });
-});
+}
+
 
 function ambience(column ,element){
-    if(column === 2 ) element.classList.add("grass");
+    if(column === 2 ) element.classList.add("Darkgrass");
     if(column === 3 ) element.classList.add("Darkgrass");
     if(column === 1 ) element.classList.add("sky");
+    if(column === 6 ) grass(element) 
+}
+
+function enemy(element){
+    let img = document.createElement("img");
+    element.classList.add("Darkgrass");
+    img.src = "images/geodude.gif"
+    img.style.width = "130px"
+    img.style.height = "110px"
+    element.appendChild(img)
+}
+
+function characters(column,element){
+    if(column === 4 ) Pikachu(element)
+    if(column === 5 ) enemy(element)
 }
 
 function Pikachu(element) {
@@ -39,11 +73,27 @@ function Pikachu(element) {
     element.appendChild(img)
 }
 
-function Hitch(element) {
+function grass(element) {
     let img = document.createElement("img");
-    element.classList.add("Darkgrass");
-    img.src = "images/geodude.gif"
-    img.style.width = "130px"
+    element.classList.add("sky");
+    element.classList.add("b");
+    img.src = "images/grass-image.png"
+    img.style.width = "144px"
     img.style.height = "110px"
     element.appendChild(img)
 }
+
+
+
+setInterval(game, 1000);
+
+window.addEventListener("keydown", (event) => {
+         if(event.key = "ArrowUp") {
+            let PikachuRowPosition = Matrix[Matrix.length-1]
+            let PikachuColumnPosition = PikachuRowPosition[1]
+
+            let upperrow = Matrix[Matrix.length-1]
+            let uppercolumn = upperrow[1]
+            console.log(uppercolumn)
+         }
+  });
